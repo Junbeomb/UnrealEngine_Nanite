b63661442611528b329fd54ee3185c3aa3850fde
junn216 <junbeomb216@gmail.com>
2024-03-25 13:03:40 +0900

    Add FoliageRockBase

---
+++ Source/NaniteCPP/FoliageRockBase.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FoliageInfluencer.h"
+#include "FoliageRockBase.generated.h"
+
+UCLASS()
+class NANITECPP_API AFoliageRockBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFoliageRockBase();
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	//월드에 있는 FoliageInfluencers 정보들
+	UPROPERTY(EditAnywhere, category = "Physics")
+	TArray<AActor*> AllFoliageInfluencers;
+	UPROPERTY(EditAnywhere, category = "Physics")
+	int FoliageInfluencersLen;
+	UPROPERTY(EditAnywhere, category = "Physics")
+	bool IsInfluencersInRange;
+	UFUNCTION()
+	void NoInfluencersInRangeFunc();
+
+	//월드에있는 Foliage 가져오기
+	class AInstancedFoliageActor* WorldFoliage;
+
+	//FoliageInfluencer가 주위에 있는지 확인
+	UFUNCTION()
+	void checkToFoliageInfluencer();
+
+	UPROPERTY()
+	bool DoOnce;
+
+	UPROPERTY(EditAnywhere)
+	bool FinishDoOnce;
+
+};
