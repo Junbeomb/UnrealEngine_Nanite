e9e0db99e3ed10405ca98cb75c0415205ad19561
junn216 <junbeomb216@gmail.com>
2024-04-19 14:42:20 +0900

    Input Action Change

---
+++ Source/NaniteCPP/Blending/MaterialChangeBall.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MaterialChangeBall.generated.h"
+
+UCLASS()
+class NANITECPP_API AMaterialChangeBall : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* StaticMesh;
+	
+public:	
+	// Sets default values for this actor's properties
+	AMaterialChangeBall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
