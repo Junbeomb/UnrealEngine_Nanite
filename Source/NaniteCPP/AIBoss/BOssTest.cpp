4c8fdba8500e470bd1fdde0f6dc8dff80ae8fe90
junn216 <junbeomb216@gmail.com>
2024-06-23 20:35:53 +0900

    Add BOssTest

--- Source/NaniteCPP/AIBoss/BOssTest.cpp
+++ Source/NaniteCPP/AIBoss/BOssTest.cpp
@@ -2,33 +2,155 @@
 
 
 #include "BOssTest.h"
+#include "Comp_AIDamageSystem.h"
+#include "Comp_AIBossAttackSystem.h"
+#include "Components/CapsuleComponent.h"
+#include "AIController.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "../Blending/Comp_BlendMesh.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "AIC_BossBase.h"
+#include "BossAttackStructData.h"
+#include "Kismet/GameplayStatics.h"
+
+
 
 // Sets default values
 ABOssTest::ABOssTest()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	CapsuleComp = ACharacter::GetCapsuleComponent();
+	RootComponent = CapsuleComp;
+
+	SkeletalMesh = ACharacter::GetMesh();
+	SkeletalMesh->SetupAttachment(RootComponent);
+
+	Comp_Attack = CreateDefaultSubobject<UComp_AIBossAttackSystem>(TEXT("Comp_Attack"));
+	Comp_Damage = CreateDefaultSubobject<UComp_AIDamageSystem>(TEXT("Comp_Damage"));
+	Comp_Blend = CreateDefaultSubobject<UComp_BlendMesh>(TEXT("Comp_Blend"));
+
+	//Dissolve Timeline
+	DissolveTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("DissolveTimeline"));
+	//callback함수bind
+	floatTimelineCallback.BindUFunction(this, FName("DissolveTimelineUpdate"));
+	floatTimelineFinishedCallback.BindUFunction(this, FName("DissolveTimelineFinish"));
+
+	//aicontroller 설정
+	AIControllerClass = AAIC_BossBase::StaticClass();
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 
 }
+
+
 
 // Called when the game starts or when spawned
 void ABOssTest::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//ACharacter* c = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+	////AttackCombo1(nullptr);
+	////ThrowBall(nullptr);
+	//JumpAttack(Cast<AActor>(c));
+
+	//if (!BehaviorTree) {
+	//	UE_LOG(LogTemp, Warning, TEXT("BT Empty"));
+	//	return;
+	//}
+
+	////UE_LOG(LogTemp, Warning, TEXT("%s"), GetController()->GetName());
+
+	//for (int i = 0; i < SkeletalMesh->GetNumMaterials(); i++) {
+	//	 UMaterialInstanceDynamic* TempDMI = SkeletalMesh->CreateDynamicMaterialInstance(i, SkeletalMesh->GetMaterial(i));
+	//	 DMIList.Add(TempDMI);
+	//}
+
+	//Comp_Damage->D_OnDeath.BindUObject(this, &AAIBossBase::Die);
+	//Comp_Damage->D_OnDamageResponse.BindUObject(this, &AAIBossBase::HitResponse);
+
 }
 
-// Called every frame
-void ABOssTest::Tick(float DeltaTime)
+void ABOssTest::Die()
 {
-	Super::Tick(DeltaTime);
+	Comp_Blend->StartBlend();
+	CapsuleComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	//AIC_BossBase->StopMovement();
+	GetController()->StopMovement();
 
+	SkeletalMesh->SetSimulatePhysics(true);
+
+	FTimerHandle DestroyTimerHandle;
+	FTimerDelegate TimerDelegate;
+	TimerDelegate.BindLambda([&]
+		{
+			DissolveTimeline->AddInterpFloat(DissolveCurve, floatTimelineCallback, FName("DissolveAmountTL"));
+			DissolveTimeline->SetTimelineFinishedFunc(floatTimelineFinishedCallback);
+			DissolveTimeline->PlayFromStart();
+		});
+
+	GetWorld()->GetTimerManager().SetTimer(DestroyTimerHandle, TimerDelegate, 3, false);
+}
+
+void ABOssTest::DissolveTimelineFinish()
+{
+	UE_LOG(LogTemp, Warning, TEXT("AIBossBase Dissolve Timeline Finish"));
+}
+
+void ABOssTest::DissolveTimelineUpdate(float Value)
+{
+	for (UMaterialInstanceDynamic* a : DMIList) {
+		a->SetScalarParameterValue(FName("DissolveAmount"), Value);
+	}
+}
+
+void ABOssTest::HitResponse()
+{
+	GetCharacterMovement()->StopMovementImmediately();
 }
 
 // Called to bind functionality to input
 void ABOssTest::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
 
+
+void ABOssTest::AttackCombo1(AActor* ATarget)
+{
+	FBOSSATTACKDATA TempAData;
+	TempAData.AttackTarget = ATarget;
+	TempAData.DamageAmount = 11;
+	TempAData.radius = 30.f;
+	TempAData.length = 50.f;
+	TempAData.Montage = Combo1Montage;
+	UE_LOG(LogTemp, Warning, TEXT("AttackCombo1() in AIBossBase.cpp"));
+	Comp_Attack->BossPrimaryAttack(TempAData);
+}
+
+void ABOssTest::ThrowBall(AActor* ATarget)
+{
+	FBOSSATTACKDATA TempAData;
+	TempAData.AttackTarget = ATarget;
+	TempAData.DamageAmount = 20;
+	TempAData.radius = 30.f;
+	TempAData.length = 50.f;
+	TempAData.Montage = ThrowBallMontage;
+	UE_LOG(LogTemp, Warning, TEXT("ThrowBall() in AIBossBase.cpp"));
+	Comp_Attack->BossThrowBall(TempAData);
+}
+
+void ABOssTest::JumpAttack(AActor* ATarget)
+{
+	FBOSSATTACKDATA TempAData;
+	TempAData.AttackTarget = ATarget;
+	TempAData.DamageAmount = 20;
+	TempAData.radius = 30.f;
+	TempAData.length = 50.f;
+	TempAData.Montage = JumpAttackMontage;
+	UE_LOG(LogTemp, Warning, TEXT("JumpAttack() in AIBossBase.cpp"));
+	Comp_Attack->BossJumpAttack(TempAData);
 }
 
--- Source/NaniteCPP/AIBoss/BOssTest.h
+++ Source/NaniteCPP/AIBoss/BOssTest.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Components/TimelineComponent.h"
 #include "BOssTest.generated.h"
 
 UCLASS()
@@ -11,17 +12,92 @@ class NANITECPP_API ABOssTest : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(EditAnywhere)
+	class UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* SkeletalMesh;
+
+
+
+	UPROPERTY(EditAnywhere)
+	class UComp_AIDamageSystem* Comp_Damage;
+	UPROPERTY(EditAnywhere)
+	class UComp_BlendMesh* Comp_Blend;
+
+	UPROPERTY(EditAnywhere)
+	class AAIController* AIC_BossBase;
+
+	UFUNCTION()
+	void Die();
+
+	UFUNCTION()
+	void HitResponse();
+
+
+
+	UPROPERTY(EditAnywhere)
+	TArray<UMaterialInstanceDynamic*> DMIList;
+
+
+	//=====================DissolveTimeline=========================
+	//=====================DissolveTimeline=========================
+	//=====================DissolveTimeline=========================
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	class UTimelineComponent* DissolveTimeline;
+
+	//DissolveTimeline 종료시
+	class FOnTimelineEvent floatTimelineFinishedCallback;
+	UFUNCTION()
+	void DissolveTimelineFinish();
+
+	UPROPERTY(EditAnywhere, Category = "Timeline");
+	class UCurveFloat* DissolveCurve;
+	//callback함수
+	class FOnTimelineFloat floatTimelineCallback;
+	//Bind함수
+	UFUNCTION()
+	void DissolveTimelineUpdate(float Value);
+
+	//=====================DissolveTimeline=========================
+	//=====================DissolveTimeline=========================
+	//=====================DissolveTimeline=========================
+
+
+
+	UPROPERTY(EditAnywhere)
+	class UAnimMontage* Combo1Montage;
+
+	UPROPERTY(EditAnywhere)
+	class UAnimMontage* ThrowBallMontage;
+
+	UPROPERTY(EditAnywhere)
+	class UAnimMontage* JumpAttackMontage;
+
 public:
 	// Sets default values for this character's properties
 	ABOssTest();
+
+	UPROPERTY(EditAnywhere)
+	class UComp_AIBossAttackSystem* Comp_Attack;
+
+	UPROPERTY(EditAnywhere)
+	class UBehaviorTree* BehaviorTree;
+
+	UFUNCTION()
+	void AttackCombo1(AActor* ATarget);
+
+	UFUNCTION()
+	void ThrowBall(AActor* ATarget);
+
+	UFUNCTION()
+	void JumpAttack(AActor* ATarget);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
