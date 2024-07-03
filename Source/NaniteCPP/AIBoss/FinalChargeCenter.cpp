ed30d15f5cc9d001f31be793ea706d5df645e5fa
junn216 <junbeomb216@gmail.com>
2024-07-04 01:42:54 +0900

    Continue Timeline in FinalChargeCenter

--- Source/NaniteCPP/AIBoss/FinalChargeCenter.cpp
+++ Source/NaniteCPP/AIBoss/FinalChargeCenter.cpp
@@ -1,6 +1,7 @@
 #include "FinalChargeCenter.h"
 #include "NiagaraFunctionLibrary.h"
 #include "NiagaraComponent.h"
+#include "../Blending/Comp_BlendMesh.h"
 
 
 AFinalChargeCenter::AFinalChargeCenter()
@@ -12,6 +13,13 @@ AFinalChargeCenter::AFinalChargeCenter()
 
 	Niagara = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Niagara"));
 
+	Comp_Blend = CreateDefaultSubobject<UComp_BlendMesh>(TEXT("Comp_Blend"));
+
+	//TL_Dissapear
+	TL_Dissapear = CreateDefaultSubobject<UTimelineComponent>(TEXT("TL_Dissapear"));
+	EmissiveCallback.BindUFunction(this, FName("EmissiveUpdate"));
+	WPOCallback.BindUFunction(this, FName("WPOUpdate"));
+	TLDissapearFinishCallback.BindUFunction(this, FName("TLDissapearFinish"));
 }
 
 
@@ -19,6 +27,44 @@ void AFinalChargeCenter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	DMI = CenterMesh->CreateDynamicMaterialInstance(0, CenterMesh->GetMaterial(0));
+
+	Comp_Blend->D_StartBlending.BindUObject(this, &AFinalChargeCenter::BlendStart);
+	Comp_Blend->D_FinishBlending.BindUObject(this, &AFinalChargeCenter::BlendFinish);
+}
+
+void AFinalChargeCenter::BlendStart()
+{
+	UE_LOG(LogTemp, Warning, TEXT("FinalChargeCenter : Blend Start"));
+
+	//ScaleTimeline
+	if (EmissiveCurve && WPOCurve) {
+		TL_Dissapear->AddInterpFloat(EmissiveCurve, EmissiveCallback, FName("Emissive"));
+		TL_Dissapear->AddInterpFloat(WPOCurve, WPOCallback, FName("WPO"));
+
+		TL_Dissapear->SetTimelineFinishedFunc(TLDissapearFinishCallback);
+		TL_Dissapear->PlayFromStart();
+	}
+}
+
+void AFinalChargeCenter::BlendFinish()
+{
+	UE_LOG(LogTemp, Warning, TEXT("FinalChargeCenter : Blend Finish"));
+
+}
+void AFinalChargeCenter::EmissiveUpdate(float Value)
+{
+	DMI->SetScalarParameterValue(FName("Emissive"),Value);
+}
+
+void AFinalChargeCenter::WPOUpdate(float Value)
+{
+	DMI->SetScalarParameterValue(FName("WPO"), Value);
+}
+
+void AFinalChargeCenter::TLDissapearFinish()
+{
+	UE_LOG(LogTemp, Warning, TEXT("FinalChargeCenter : TL Finish"));
 }
 
 void AFinalChargeCenter::Tick(float DeltaTime)
--- Source/NaniteCPP/AIBoss/FinalChargeCenter.h
+++ Source/NaniteCPP/AIBoss/FinalChargeCenter.h
@@ -3,6 +3,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
+#include "Components/TimelineComponent.h"
+
 #include "FinalChargeCenter.generated.h"
 
 UCLASS()
@@ -15,6 +18,45 @@ class NANITECPP_API AFinalChargeCenter : public AActor
 
 	UPROPERTY(EditAnywhere)
 	class UNiagaraComponent* Niagara;
+
+	UPROPERTY()
+	UMaterialInstanceDynamic* DMI;
+
+
+private://Blend
+	UPROPERTY()
+	class UComp_BlendMesh* Comp_Blend;
+
+	UFUNCTION()
+	void BlendStart();
+	UFUNCTION()
+	void BlendFinish();
+
+private://Timeline
+
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	UTimelineComponent* TL_Dissapear;
+
+	//TL_Dissapear
+		//TL_Dissapear 종료시
+		FOnTimelineEvent TLDissapearFinishCallback;
+		UFUNCTION()
+		void TLDissapearFinish();
+
+	UPROPERTY(EditAnywhere, Category = "Timeline");
+	class UCurveFloat* EmissiveCurve;
+	UPROPERTY(EditAnywhere, Category = "Timeline");
+	class UCurveFloat* WPOCurve;
+	//callback함수
+	FOnTimelineFloat EmissiveCallback;
+	//callback함수
+	FOnTimelineFloat WPOCallback;
+	//Bind함수
+	UFUNCTION()
+	void EmissiveUpdate(float Value);
+	UFUNCTION()
+	void WPOUpdate(float Value);
+
 	
 public:	
 	AFinalChargeCenter();
