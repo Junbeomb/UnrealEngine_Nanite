1074e8fe086a7fa0854ced5be5331890599e566a
junn216 <junbeomb216@gmail.com>
2024-08-25 05:43:33 +0900

    Refactor PuzzleKey

--- Source/NaniteCPP/PuzzleSystem/Key/PuzzleKey.cpp
+++ Source/NaniteCPP/PuzzleSystem/Key/PuzzleKey.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "PuzzleKey.h"
 #include "../../Blending/Comp_BlendMesh.h"
@@ -7,35 +5,18 @@
 #include "Kismet/GameplayStatics.h"
 #include "PuzzleKeyPad.h"
 
-
-
-// Sets default values
 APuzzleKey::APuzzleKey()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
 	KeyStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("KeyStaticMesh"));
 	RootComponent = KeyStaticMesh;
 	
 	Comp_Blend = CreateDefaultSubobject<UComp_BlendMesh>(TEXT("Comp_Blend"));
-
-}
-
-// Called when the game starts or when spawned
-void APuzzleKey::BeginPlay()
-{
-	Super::BeginPlay();
-
 	Comp_Blend->D_FinishBlending.BindUObject(this, &APuzzleKey::OnFinishBlending);
-
-	//Comp_Blend->D_FinishBlending.Execute();
-
 }
 
+//blending이 끝나면 상호작용 활성화
 void APuzzleKey::OnFinishBlending()
 {
-	//interact component 추가
 	Comp_Interact =Cast<UComp_InteractBase>(AddComponentByClass(UComp_InteractBase::StaticClass(), false, FTransform::Identity, false));
 	if (Comp_Interact) {
 		Comp_Interact->RegisterComponent();
@@ -44,16 +25,8 @@ void APuzzleKey::OnFinishBlending()
 
 void APuzzleKey::PressEStart()
 {
-	//UE_LOG(LogTemp, Warning, TEXT("PressEStart"));
 	APuzzleKeyPad* KeyPad = Cast<APuzzleKeyPad>(UGameplayStatics::GetActorOfClass(GetWorld(), APuzzleKeyPad::StaticClass()));
 	KeyPad->GainKey();
 	Destroy();
-}
-
-// Called every frame
-void APuzzleKey::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
--- Source/NaniteCPP/PuzzleSystem/Key/PuzzleKey.h
+++ Source/NaniteCPP/PuzzleSystem/Key/PuzzleKey.h
@@ -14,28 +14,22 @@ class NANITECPP_API APuzzleKey : public AActor, public IInterface_Interact
 
 	UPROPERTY(EditAnywhere)
 	UStaticMeshComponent* KeyStaticMesh;
-
-	UPROPERTY(EditAnywhere)
-	class UComp_BlendMesh* Comp_Blend;
-
-	UPROPERTY(EditAnywhere)
+	//알아서 메모리 관리
+	 //AddComponentByClass 로 값을 초기화 하므로 UPROPERTY() 안붙여도 됨. PuzzleKeyPad에 사용된 NewOBject는 붙여야됨.
 	class UComp_InteractBase* Comp_Interact;
+	//CreateDefaultSubobject 로 값을 초기화 하므로 UPROPERTY() 안붙여도 됨.
+	class UComp_BlendMesh* Comp_Blend; 
 
-	UFUNCTION()
+
+	//UFUNCTION()을 붙이지 않아도 됨.
+	//타임라인의 Binding과는 다르게 C++에서 바인드 하는 함수이므로
+	//타임라인의 Binding는 언리얼 엔진의 특수기능이라서 UFUNCTION() 을 붙여주어야 Binding 될 수 있음. (리플렉션 시스템)
 	void OnFinishBlending();
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	APuzzleKey();
 
 	virtual void PressEStart() override;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
